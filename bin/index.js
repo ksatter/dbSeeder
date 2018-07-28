412bc82521c48bb1a4a769a3448220878a3dbeec
Kathy Satterlee <me@ksatter.com>
2018-07-28 09:56:25 -0400

    Update

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+var path = require("path");
 
 var app = require('../app.js');
 
+var cwd = process.cwd();
+console.log(cwd) 
+//var file = path.join(cwd, process.argv[2]);
 
-
-app.run()
+app.run(file)
