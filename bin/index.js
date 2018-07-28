1f3d2d2ac155f0ced4b86e59e9fa6baa2572cb49
Kathy Satterlee <me@ksatter.com>
2018-07-28 14:44:31 -0400

    Clean Start

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,12 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
+
 var path = require('path');
 var app = require('./../app');
+console.log(process.argv)
 
+// var cwd = process.cwd();
+// var filePath = path.join(cwd, process.argv[2]);
 
-var cwd = process.cwd();
-var filePath = path.join(cwd, process.argv[2]);
+// console.log(filePath + "**");
 
-console.log(filePath + "**");
-
-app(filePath);
+// app(filePath);
