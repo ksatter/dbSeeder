942fdb4b899879337874759590e0ad9d426e798f
Kathy Satterlee <me@ksatter.com>
2018-07-28 04:31:51 -0400

    Adding bin

---
+++ app.js
@@ -0,0 +1,22 @@
+var execSQL = require("exec-sql");
+var path = require("path")
+
+execSQL.connect({
+    'database': 'mysql',
+    'user': 'root',
+    'password': '1113406'
+});
+
+// execSQL.executeFile(process.argv[2])
+
+
+
+function run(file){
+    execSQL.executeFile(file, function(err){
+        if (err) throw err;
+        execSQL.disconnect();
+        console.log('Done!');
+    })
+}
+
+module.exports = run
