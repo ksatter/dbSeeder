709e3c661432e6d1a04998aff711d982f8c2cdb1
Kathy Satterlee <me@ksatter.com>
2018-07-28 13:19:38 -0400

    u

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,11 @@
 // #!/usr/bin/env node
-var app = require('./../app');
 var path = require('path');
+var app = require('./../app');
+
 
 var cwd = process.cwd();
 var filePath = path.join(cwd, process.argv[2]);
 
 console.log(filePath + "**");
 
-app(filePath);
+//app(filePath);
